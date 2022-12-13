08076eee84b90fdd9ff2032e82c3307d1d42b116
Krista Cannady <kristalcannady@gmail.com>
2022-12-12 20:22:38 -0500

    webpack config

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,11 +11,36 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // connect to DB and version we want to use
+  const jateDb = await openDB('jate', 1);
+  // make new transaction, specify the DB we are posting to and the data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open the object store
+  const store = tx.objectStore('jate');
+  // pass in content
+  const request = store.put({ id: 1, value: content });
+  // confirm the data was added
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  // connect to DB and version we want to use
+  const jateDB = await openDB('jate', 1);
+  // create a new transaction and specify the store and data privileges
+  const tx = jateDB.transaction('jate', 'readonly');
+  // open up the desired object store
+  const store = tx.objectStore('jate');
+  // get all data in the database
+  const request = store.getAll();
+  // get confirmation of the request
+  const result = await request;
+  console.log(result);
+  return result;
+};
 
 initdb();
